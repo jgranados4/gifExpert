edaf8914a13374bf6d83036f4ca1f1bf5378e64c
jgranados4 <jgranados@est.ups.edu.ec>
2022-10-16 21:26:48 -0500

    feat: :sparkles: Agregamos hooks

--- GifExperto/src/componentes/GifGrid.jsx
+++ GifExperto/src/componentes/GifGrid.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import { getGifs } from '../helpers/getGifs'
+
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GifItem } from './GifItem'
 
 export const GifGrid = ({category}) => {
-  getGifs(category);
+  const {imagen,isLoading}=useFetchGifs(category);
+
   return (
     <>
     <h3>{category}</h3>
+    <div className="card-grid" >
+      {/* Recorrer con map  */}
+       { imagen.map((image)=>(
+          <GifItem
+           key={image.id}
+           {...image}
+           />
+        ))
+       }
+    </div>
     </>
   )
 }
